c6527f95f3fee447c9561ca1c018bdcfc4cd7ca4
4iki93@gmail.com <love0412>
2017-10-18 16:20:46 +0300

    JPA work

--- src/main/java/ua/com/owu/Main.java
+++ src/main/java/ua/com/owu/Main.java
@@ -1,13 +1,34 @@
 package ua.com.owu;
 
+import ua.com.owu.entity.Room;
+import ua.com.owu.entity.User;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         EntityManagerFactory factory= Persistence.createEntityManagerFactory("okten");
         EntityManager manager = factory.createEntityManager();
+
+
+        manager.getTransaction().begin();
+        //manager.persist(new User("vasya","pupkin"));
+        //System.out.println(manager.find(User.class, 1));
+
+
+        List<Room> rooms = new ArrayList<>();
+        rooms.add(new Room (12));
+        rooms.add(new Room (45));
+        rooms.add(new Room (86));
+        User user = manager.find(User.class, 1);
+        user.setRooms(rooms);
+        manager.persist(user);
+        manager.getTransaction().commit();
+
 
 
         manager.close();
